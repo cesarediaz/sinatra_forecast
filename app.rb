36da8d18c123ddf8cdb0f7192c9af647342a332d
Cesar Diaz <cesar@mediyak.com>
2016-02-17 10:27:54 -0300

    added call to html helpers

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
 require './routes'
 
+require 'sinatra/support/countryhelpers'
+require 'sinatra/support/htmlhelpers'
+
 set :root, File.dirname(__FILE__)
 set :views, settings.root + '/views'
+
+helpers Sinatra::HtmlHelpers
+helpers Sinatra::CountryHelpers
