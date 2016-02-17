2152817d9e2e9b3202fbbb0fc8c087579e7b2cde
Cesar Diaz <cesar@mediyak.com>
2016-02-17 18:49:28 -0300

    moving all requires inside main config file

--- app.rb
+++ app.rb
@@ -3,7 +3,10 @@ require './routes'
 
 require 'sinatra/support/countryhelpers'
 require 'sinatra/support/htmlhelpers'
+require 'yaml'
+require 'open_weather'
 
+set :config, YAML::load_file(File.join(__dir__, 'config', 'config.yml'))
 set :root, File.dirname(__FILE__)
 set :views, settings.root + '/views'
 
--- routes.rb
+++ routes.rb
@@ -1,7 +1,3 @@
-require 'open_weather'
-require 'yaml'
-config = YAML::load_file(File.join(__dir__, 'config', 'config.yml'))
-
 get '/' do
   haml :"index"
 end
@@ -9,7 +5,7 @@ end
 get '/forecast' do
   @country = params['country']
   @city = params['city']
-  open_weather_options = { units: "metric", APPID: config['weather_key'], cnt: 16}
+  open_weather_options = { units: "metric", APPID: settings.config['weather_key'], cnt: 16}
   @city_weather = OpenWeather::Forecast.city("#{params['city']}, #{params['country']['name']}", open_weather_options)
   haml :"forecast", locals: {country: @country, city: @city, city_weather: @city_weather }
 end
