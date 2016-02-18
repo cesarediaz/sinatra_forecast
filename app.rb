683cf7a6d1def6762a2814f554c104e831c5990f
Cesar Diaz <cesar@mediyak.com>
2016-02-18 18:59:31 -0300

    refactoring code

--- app.rb
+++ app.rb
@@ -11,5 +11,7 @@ set :config, YAML::load_file(File.join(__dir__, 'config', 'config.yml'))
 set :root, File.dirname(__FILE__)
 set :views, settings.root + '/views'
 
+set :open_weather_options, { units: "metric", APPID: settings.config['weather_key'], cnt: 16}
+
 helpers Sinatra::HtmlHelpers
 helpers Sinatra::CountryHelpers
--- routes.rb
+++ routes.rb
@@ -5,7 +5,6 @@ end
 get '/forecast' do
   @country = params['country']
   @city = params['city']
-  open_weather_options = { units: "metric", APPID: settings.config['weather_key'], cnt: 16}
-  @city_weather = OpenWeather::Forecast.city("#{params['city']}, #{params['country']['name']}", open_weather_options)
+  @city_weather = OpenWeather::Forecast.city("#{params['city']}, #{params['country']['name']}", settings.open_weather_options)
   haml :"forecast", locals: {country: @country, city: @city, city_weather: @city_weather }
 end
