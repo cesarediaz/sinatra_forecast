72643413dbcff0e0f27e3a93413bcd411bed5126
Cesar Diaz <cesar@mediyak.com>
2016-02-21 11:39:12 -0300

    refactorized routes, added countries file

--- app.rb
+++ app.rb
@@ -1,10 +1,9 @@
 require 'sinatra'
-require './routes'
+require './routes/routes'
 
-require 'sinatra/support/countryhelpers'
-require 'sinatra/support/htmlhelpers'
 require 'yaml'
 require 'open_weather'
+require './config/countries'
 
 set :title, 'Forecast Weather'
 set :config, YAML::load_file(File.join(__dir__, 'config', 'config.yml'))
@@ -12,6 +11,3 @@ set :root, File.dirname(__FILE__)
 set :views, settings.root + '/views'
 
 set :open_weather_options, { units: "metric", APPID: settings.config['weather_key'], cnt: 16}
-
-helpers Sinatra::HtmlHelpers
-helpers Sinatra::CountryHelpers
