8add9a38ce295b8973570bd1c061207d8fa790ef
Cesar Diaz <cesar@mediyak.com>
2016-02-23 18:54:57 -0300

    added route for widget and view

--- routes/routes.rb
+++ routes/routes.rb
@@ -9,3 +9,9 @@ get '/forecast' do
   City.save(@city_weather)
   haml :"forecast", locals: {country: @country, city: @city, city_weather: @city_weather }
 end
+
+get '/widget/:id/:date' do
+  @weather = City.find_by(city_id: params['id'])
+  @date = params[:date]
+  haml :"widget", locals: {weather: @weather, date: @date}
+end
