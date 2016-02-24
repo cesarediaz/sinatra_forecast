cc329c8dc2e14b2b55f7e2afda27d32cd22057e2
Cesar Diaz <cesar@mediyak.com>
2016-02-24 11:46:17 -0300

    improved configuration for open weather api key setting

--- app.rb
+++ app.rb
@@ -8,8 +8,6 @@ require 'sinatra/activerecord'
 require './models/city.rb'
 
 set :title, 'Forecast Weather'
-set :config, YAML::load_file(File.join(__dir__, 'config', 'config.yml'))
 set :root, File.dirname(__FILE__)
 set :views, settings.root + '/views'
-
-set :open_weather_options, { units: "metric", APPID: settings.config['weather_key'], cnt: 16}
+set :open_weather_options, { units: "metric", APPID: ENV['OPEN_WEATHER_KEY'], cnt: 16}
