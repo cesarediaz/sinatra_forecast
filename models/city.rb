62b6969523d76cf55e33b2fbf8397670061896df
Cesar Diaz <cesar@mediyak.com>
2016-02-29 11:35:31 -0300

    refactorized model

--- models/city.rb
+++ models/city.rb
@@ -1,18 +1,26 @@
 class City < ActiveRecord::Base
   serialize :body, Hash
 
+  scope :find_city, -> (name, country_code, city_id) do
+    where('(name = ? AND country_code = ?) OR city_id = ?',
+          name.upcase,
+          country_code.upcase,
+          city_id)
+  end
+
   def self.save(city_weather)
     if city = find_by_city_id(city_weather['city']['id'])
       city.update_attributes(body: city_weather)
     else
-      city = city_weather['city']['name'].upcase
-      country_code = city_weather['city']['country'].upcase
-      create(city_id: city_weather['city']['id'], body: city_weather, name: city, country_code: country_code)
+      create(city_id: city_weather['city']['id'],
+             body: city_weather,
+             name: city_weather['city']['name'].upcase,
+             country_code: city_weather['city']['country'].upcase)
     end
   end
 
   def self.exist?(city_id = nil, name = nil, country_code = nil)
-    city = where('(name = ? AND country_code = ?) OR city_id = ?', name.upcase, country_code.upcase, city_id).first
+    city = find_city(name, country_code, city_id).first
     city ? recently_updated?(city) : false
   end
 
